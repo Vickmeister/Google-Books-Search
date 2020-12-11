3459425f504dd1268e406015a164fa4d1019a9ea
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-12-11 12:33:25 -0600

    updated index api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,4 +3,6 @@ const bookRoutes = require("./books");
 
 router.use("/books", bookRoutes);
 
+router.use(function(req, res) {  res.sendFile(path.join(__dirname, "../../client/build/index.html"));});
+
 module.exports = router;
